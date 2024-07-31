4c9773d7734fabdde203fce531b77d0f37faf516
Tommy Huang <tommyhuang999@gmail.com>
2024-07-31 16:19:32 -0400

    testing different dodging mechanics

--- shipgame/player.gd
+++ shipgame/player.gd
@@ -1,35 +1,47 @@
 extends Area2D
 signal hit
 
-@export var mass = 1000 # "average" ship will have mass = 1000
+@export var mass = 1200 # "average" ship will have mass = 1000
 @export var thrust = 800 # "average" ship will have thrust = 800
-@export var boost = 2400 # max boost
-
-var drag = 0.75 # percentage "air resistance"
-var fric = 20 + mass * 0.02 # constant decceleration
-
-var max_speed = max(600 + (thrust - mass) * 0.1, 300) # How fast the player will move (pixels/sec).
-var accel = thrust * 1000 / mass # nominal acceleration (pixels/sec^2)
-var size_control_penalty = max(pow(100 + mass * 0.005, 0.5) - 9, 1)
-var control = (2 * PI) * thrust / (mass * size_control_penalty) # max rotation speed (radians/sec)
-
+var drag
+var fric
+var max_speed
+var accel
+var size_control_penalty
+var control
 var vel # current (x, y) velocity
+
 var screen_size # Size of the game window.
+
+@export var dodge_speed = 700 # max dodge boost
+@export var dodge_duration = 2. # decays over this duration
+var curr_dodge_vec = Vector2.ZERO
+var dodge_decay_factor = 0. # starts at 1 when dodging, decays to 0 over duration
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	
+	drag = 0.75 # percentage "air resistance"
+	fric = 20 + mass * 0.02 # constant decceleration
+
+	max_speed = max(600 + (thrust - mass) * 0.1, 300) # How fast the player will move (pixels/sec).
+	accel = thrust * 1000 / mass # nominal acceleration (pixels/sec^2)
+	size_control_penalty = max(pow(100 + mass * 0.005, 0.5) - 9, 1)
+	control = (2 * PI) * thrust / (mass * size_control_penalty) # max rotation speed (radians/sec)
+	
 	vel = Vector2(0, 0)
-	#print("BASE STATS")
-	#print("mass: ", mass)
-	#print("thrust: ", thrust)
-	#print("\nDERIVED STATS")
-	#print("drag: ", drag)
-	#print("friction: ", fric)
-	#print("max_speed: ", max_speed)
-	#print("accel: ", accel)
-	#print("control penalty:", size_control_penalty)
-	#print("control: ", control)
+	
+	print("BASE STATS")
+	print("mass: ", mass)
+	print("thrust: ", thrust)
+	print("\nDERIVED STATS")
+	print("drag: ", drag)
+	print("friction: ", fric)
+	print("max_speed: ", max_speed)
+	print("accel: ", accel)
+	print("control penalty:", size_control_penalty)
+	print("control: ", control)
 
 # slides a towards b by between n and 2n, smoothed
 func slide(a, b, peak, n):
@@ -67,18 +79,24 @@ func accelerate(delta):
 	accel_vector = accel_vector.rotated(self.get_rotation())
 	
 	var effective_accel = accel
-	var boost_max_speed_bonus = 0
-	if Input.is_action_pressed("boost"):
-		effective_accel += boost
-		boost_max_speed_bonus = 200
 	vel.x += accel_vector.x * effective_accel * delta
 	vel.y += accel_vector.y * effective_accel * delta
 	
-	if vel.length() > max_speed + boost_max_speed_bonus:
-		vel *= (max_speed + boost_max_speed_bonus) / vel.length()
-	
+	if vel.length() > max_speed:
+		vel *= (max_speed) / vel.length()
 	position += vel * delta
-	print(vel.length()  )
+	
+	# calc dodge separately from normal velocity
+	if Input.is_action_pressed("boost") and dodge_decay_factor == 0:
+		dodge_decay_factor = 1
+		if accel_vector.length() > 0:
+			curr_dodge_vec = accel_vector * dodge_speed
+		else:
+			curr_dodge_vec = transform.x * dodge_speed
+	position += curr_dodge_vec * dodge_decay_factor * delta
+	if dodge_decay_factor > 0:
+		dodge_decay_factor -= (delta / dodge_duration)
+		dodge_decay_factor = max(dodge_decay_factor, 0)
 
 func _process(delta):
 	turn(delta)
--- shipgame/weapon_basic_gun.gd
+++ shipgame/weapon_basic_gun.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var prop_cooldown = 0.25
+@export var prop_cooldown = 0.5
 var cooldown = 0
 
 # the projectile shot by this weapon
@@ -17,7 +17,7 @@ func _process(delta):
 		cooldown = prop_cooldown
 		
 		var shot_instance = shot_scene.instantiate()
-		shot_instance.speed = 1600
+		shot_instance.speed = 2000
 		shot_instance.lifetime = 1.2
 		shot_instance.rotation = global_rotation
 		shot_instance.position = global_position
